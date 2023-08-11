65ef322c4d4a4efef711090a444e2b33c9ed35b6
NixOS <konecho@outlook.com>
2023-08-11 22:42:07 +0800

    compatible with iverilog

--- led.v
+++ led.v
@@ -1,23 +1,25 @@
 //plug pmod-led on PMOD2
 
-module switch(input CLK, output [7:0] LED);
-      
-    reg [25:0] counter;
-    wire lclk = counter[21];
+module led (
+    input CLK,
+    output reg [7:0] LED
+);
 
-    initial begin
-        LED = 8'b11111110;
-    end
+  reg [25:0] counter;
+  wire lclk = counter[21];
 
-    always @(posedge CLK)
-    begin
-        counter <= counter + 1;
-    end
+  initial begin
+    LED = 8'b11111110;
+    counter = 26'b0;
+  end
 
-    always @(posedge lclk)
-    begin
-        LED <= {LED[6:0], LED[7]};
-    end
+  always @(posedge CLK) begin
+    counter <= counter + 1;
+  end
+
+  always @(posedge lclk) begin
+    LED <= {LED[6:0], LED[7]};
+  end
 
 
 endmodule
